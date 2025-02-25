d5765d9790c81f1788004e7418922374081c8467
Aniket Madhe <aniketmadhe25@gmail.com>
2025-02-25 15:40:01 +0530

    updated urls

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://bookworm-9.onrender.com",
+    origin: "https://e-com-bookworm-onab.onrender.com",
     credentials: true,
   })
 );
--- frontend/src/configUrl.js
+++ frontend/src/configUrl.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://bookworm-f8wq.onrender.com";
+const BASE_URL = "https://e-com-bookworm.onrender.com";
 
 export default BASE_URL;
